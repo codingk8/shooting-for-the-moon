b51b09e211369155ec154de74e955a3b11a7a7ce
codingk8 <codingk8@gmail.com>
2019-04-12 13:16:02 +0200

    updated js file

--- script.js
+++ script.js
@@ -2,49 +2,38 @@
   Made with code and care in Paris by Codingk8 - 2019 - Twitter: @codingk8
   Project made for the freeCodeCamp curriculum, "Responsive Web Design" Certification
   Project #1 - Tribute Page
-  Title: Shooting for the moon - A tribute page to Katherine G. Johnson
+  Title: Shooting for the moon - A tribute page to Katherine Johnson
 ------------------------------------------------------------------------ */
 
+/* --------------------------------------------------------------------------------------- */
+/* --- jQuery Fade in effect on main title => --- */
+/* --------------------------------------------------------------------------------------- */
+
+$(document).ready(function () { $('#header-div').fadeIn(3000); });
+
+/* Rajouter un effet fadeIn fadeOut on mousse enter et mousse leave */
 
 /* --------------------------------------------------------------------------------------- */
-/* --- Makes the "back to top" arrow appear after scroll and disappear when back up => --- */
+/* --- jQuery Toggle effect on Neil Armstrong quote => --- */
 /* --------------------------------------------------------------------------------------- */
 
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 125 || document.documentElement.scrollTop > 125) {
-    document.getElementById("btticon").style.display = "block";
-  } else if (document.body.scrollBottom > 125 || document.documentElement.scrollBottom > 125) {
-    document.getElementById("btticon").style.display = "none";
-  } else {
-    document.getElementById("btticon").style.display = "none";
-  }
+function toggle(hide) {
+    if (hide === 1) {
+        $('#moon-text').fadeIn(3000);
+        hide = 0;
+    }
+    else {
+        $('#moon-text').fadeOut(3000);
+        hide = 1;
+    }
+    setTimeout("toggle("+hide+")", 3000);
 }
-
-
-/* ---------------------------------------------------------- */
-/* --- jQuery Bounce effect on the "back to top" arrow => --- */
-/* ---------------------------------------------------------- */
 
 $(document).ready(function(){
-    $( "#btticon" ).on("mouseenter", function() {
-        $(this).effect( "bounce", { times: 3 }, "slow" );
-    })
-})
+    toggle(1);
+});
 
 
-/* ------------------------------------------ */
-/* --- "Light mode | Dark mode" effect => --- */
-/* ------------------------------------------ */
-
-function toggleDarkLight() {
-  let container = document.getElementById("container");
-  let currentClass = container.className;
-  container.className = currentClass == "dark" ? "light" : "dark";
-}
-
-
-/* ------------------ */
-/* End of js file ;o) */
-/* ------------------ */
+/* -------------------------- */
+/* --- End of js file ;o) --- */
+/* -------------------------- */
